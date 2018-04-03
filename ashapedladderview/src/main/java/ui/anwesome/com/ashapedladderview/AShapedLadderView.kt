324a948abd6fca498ef047af7968a09bcfaa42e7
Anwesh43 <anweshthecool0@gmail.com>
2018-04-03 09:34:36 +0530

    adding AShapedLadderView class

--- ashapedladderview/src/main/java/ui/anwesome/com/ashapedladderview/AShapedLadderView.kt
+++ ashapedladderview/src/main/java/ui/anwesome/com/ashapedladderview/AShapedLadderView.kt
@@ -3,3 +3,25 @@ package ui.anwesome.com.ashapedladderview
 /**
  * Created by anweshmishra on 03/04/18.
  */
+
+import android.view.*
+import android.content.*
+import android.graphics.*
+
+class AShapedLadderView (ctx : Context) : View (ctx) {
+
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw (canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent (event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
