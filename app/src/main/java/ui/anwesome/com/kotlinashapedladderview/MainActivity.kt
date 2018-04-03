884de7d358dcf556e92c626885b39580af7a88a8
Anwesh43 <anweshthecool0@gmail.com>
2018-04-03 10:17:54 +0530

    adding view in Activity

--- app/src/main/java/ui/anwesome/com/kotlinashapedladderview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinashapedladderview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinashapedladderview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.ashapedladderview.AShapedLadderView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AShapedLadderView.create(this)
     }
 }
